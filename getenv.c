9c698fd5a9012668c4c0f60c9ed4da342dfe9a30
ezesilva95 <4381@holbertonschool.com>
2022-04-04 13:48:32 -0700

    getenv

---
+++ getenv.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/*
+
+
+*/
+int _getenv(char *name)
+{:
+	int i = 0;
+	int j = 0;
+	int flag = 1;
+
+	while (environ[i])
+	{
+		if (name[0] == environ[i][0])
+		{
+			flag = 1;
+			for (j = 0; name[j]; j++)
+			{
+				if (name[j] != environ[i][j])
+				{	
+					flag = 0;
+				}
+			}
+			if (flag && environ[i][j] == '=')
+			{
+				return (i);
+			}
+		}
+		i++;
+	}
+	return (-1);
+}
